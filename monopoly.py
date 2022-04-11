c3f39ecafc2c8564f79d6c76334b89cabd3c4401
Nathan Kee <n810@Nathans-MacBook-Pro.local>
2022-04-10 18:43:16 -0700

    First Push

---
+++ monopoly.py
@@ -0,0 +1,18 @@
+import random
+
+def chanceSpace():
+    """
+    Movement Cards:
+    1) Advance to Go
+    2) Advance to Illinois Avenue (Last Red Spot)
+    3) Advance to St. Charles Palace (First Pink)
+    4) Advance to nearest Utility
+    5) Advance to nearest Railroad 
+    6) Go back 3 spaces
+    7) Go to jail
+    """
+
+def treasureSpace():
+
+
+
