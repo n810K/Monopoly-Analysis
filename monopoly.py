8a5427893df8d9dbf9aa29fc55836af3f8c57a10
Nathan <nathank1216@yahoo.com>
2022-04-24 13:32:46 -0700

    major refactoring

--- monopoly.py
+++ monopoly.py
@@ -1,9 +1,13 @@
+from multiprocessing.dummy import current_process
 import sys
 import random
+from turtle import update
+from numpy import equal, roll
 import pandas as pd
 #40 Squares in Monopoly
 
-def chanceSpace():
+def chanceSpace(currentPosition):
+    #Return Format: bool, space; bool to indicate whether sent to jail
     """
     Movement Cards:
     1) Advance to Go (0)
@@ -16,95 +20,81 @@ def chanceSpace():
     8) Advance to nearest Railroad (5,15,25,35)
     
     There are 16 different Chance Cards
+    Chance Spots: 7,22,36 
     """
-    #Chance Spots: 7,22,36 
-    global globalPosition
-
+    updatedPosition = currentPosition
     movementCards = {1 : 0, 2 : 24, 3 : 11, 4 : 39}
     chanceCard = random.randint(1,16)
-    #print("Chance Random Roll:", chanceCard) # Print selected chance card's index
 
-    if chanceCard < 5:
-        #print("Chance Card: Move to board index", movementCards[chanceCard])
-        globalPosition = movementCards[chanceCard]
+    if chanceCard <= 4:
+        updatedPosition = movementCards[chanceCard]
 
     elif chanceCard == 5:
-        #print("Chance: Go to Jail")
-        globalPosition = 10
-        #print("Updated Position:", globalPosition)
-        return True
+        updatedPosition = 10
+        return True, updatedPosition #Sent to Jail
 
     elif chanceCard == 6:
         #print("Chance Card: Move back 3 Spaces")
-        globalPosition -= 3
+        updatedPosition = currentPosition - 3
 
-    elif chanceCard == 7:
-        #print("Chance Card: Advance to Nearest Utility")
-        if (globalPosition > 12 and globalPosition < 28):
-            globalPosition = 28
-            #print("Updated Position:", globalPosition)
-            return 1
-        elif (globalPosition > 28 or globalPosition < 12):
-            globalPosition = 12
-            #print("Updated Position:", globalPosition)
-            return 1
+    #Advance to nearest utility
+    elif chanceCard == 7: 
+        if (currentPosition > 12 and currentPosition < 28):
+            updatedPosition = 28
+
+        elif (currentPosition > 28 or currentPosition < 12):
+            updatedPosition = 12
 
     elif chanceCard == 8:
         #print("Chance Card: Advance to Nearest Railroad")
-        if (globalPosition > 5 and globalPosition < 15):
-            globalPosition = 15
+        if (currentPosition > 5 and currentPosition < 15):
+            updatedPosition = 15
             #print("Updated Position:", globalPosition)
-            return 1
-        elif (globalPosition > 15 and globalPosition < 25):
-            globalPosition = 25
+        elif (currentPosition > 15 and currentPosition < 25):
+            updatedPosition = 25
             #print("Updated Position:", globalPosition)
-            return 1
-        elif (globalPosition > 25 and globalPosition < 35):
-            globalPosition = 35
+        elif (currentPosition > 25 and currentPosition < 35):
+            updatedPosition = 35
             #print("Updated Position:", globalPosition)
-            return 1
-        elif (globalPosition > 35 or globalPosition < 5):
-            globalPosition = 5
+        elif (currentPosition > 35 or currentPosition < 5):
+            updatedPosition = 5
             #print("Updated Position:", globalPosition)
-            return 1
-
+    return False, updatedPosition
     
 
-def chestSpace():
+def chestSpace(currentPosition):
     """
     Movement Cards:
     1) Advance to GO (0)
     2) Go to Jail (10)
 
     There are 16 different Community Chest Cards
+    Chest Spots: 2, 17, 33
     """
-    #Chest Spots: 2, 17, 33
-    global globalPosition
+    updatedPosition = currentPosition
     communityCard = random.randint(1,16)
 
     if communityCard == 1:
         #print("Chest Card: Advance to GO")
-        globalPosition = 0
-    elif communityCard == 2:
-        #print("Chest Card: Go to Jail")
-        globalPosition = 10
-        #print("Updated Position:", globalPosition)
-        return True #Indicate sent to jail
-    
-    #print("Updated Position:", globalPosition)
+        updatedPosition = 0
+
+    elif communityCard == 2: #Jail Chest Card
+        updatedPosition = 10
+        return True, updatedPosition #Indicate sent to jail
+    return False, updatedPosition
 
 def diceRoll():
+    equalDice = False
     diceOne = random.randint(1,6)
     diceTwo = random.randint(1,6)
     #print("diceRolls:", diceOne, diceTwo)
-    equalDice = False
     if diceOne == diceTwo:
         equalDice = True # double is true 
     return (equalDice, (diceOne + diceTwo))
 
 def boardDictionarySetup():
     boardSpaces = {}
-    for i in range(40): #Space 41 will be IN jail 
+    for i in range(40): #Space 41 [index 40] will be IN jail 
         boardSpaces[i] = 0
     return boardSpaces
 
@@ -115,8 +105,8 @@ def diceRollStatisticsSetup():
     return diceRollStats
 
 if __name__ == "__main__":
-    gameBoard = boardDictionarySetup()
-    diceRollStats = diceRollStatisticsSetup()
+    boardStats = boardDictionarySetup()
+    diceStats = diceRollStatisticsSetup()
     globalPosition = 0
     roundCount = 0
     turnCount = 0
@@ -131,59 +121,75 @@ if __name__ == "__main__":
 
     simulationCount = int(sys.argv[1])
     
+
+
     while (roundCount < simulationCount):
-        chanceResult = False
-        chestResult = False
+        jailFlag = False
         equalFlag, rollResult = diceRoll()
-
-        globalPosition += rollResult
-        #print("Position After Roll:", globalPosition)
-        if (globalPosition >= 40):
-            globalPosition = globalPosition%40
-            roundCount += 1
-
-        turnCount += 1
-
-        diceRollStats[rollResult] += 1
-        gameBoard[globalPosition] += 1
-
+        diceStats[rollResult] += 1
         tripleDoubles[turnCount%3] = equalFlag
 
-        #print(tripleDoubles)
-        
-        if (globalPosition == 7 or globalPosition == 22 or globalPosition == 36):
-            #print("Landed on Chance")
-            chanceResult = chanceSpace()
-            gameBoard[globalPosition] += 1
-
-        if (globalPosition == 2 or globalPosition == 17 or globalPosition == 33):
-            #print("Landed on Community Chest")
-            chestResult = chestSpace()
-            gameBoard[globalPosition] += 1
-
-        if ((tripleDoubles[0] == True and len(set(tripleDoubles)) == 1) or chanceResult == True): #Check that all items in the list are True
-            
-            #print("Rolling from Jail")
+        if (tripleDoubles[0] == True and len(set(tripleDoubles)) == 1):
+            globalPosition = 10
             inJailCount += 1
-            globalPosition = 10 # Move to jail since rolling 3 doubles in a row is speeding
             for i in range(3):
                 equalFlag, rollResult = diceRoll()
-                turnCount += 1
-                #print("Jail Roll", i, "", equalFlag)
-                if equalFlag == True:
-                    globalPosition += rollResult
+                if (equalFlag == True):
                     break
+            if (equalFlag == False):
+                equalFlag, rollResult = diceRoll()
+            globalPosition += rollResult
+        
+        globalPosition += rollResult
+
+        if (globalPosition > 39):
+            globalPosition = globalPosition%40
+            roundCount += 1
+        
+        boardStats[globalPosition] += 1
+
+        if (globalPosition == 7 or globalPosition == 22 or globalPosition == 36): #Chance Cards
+            currentPosition = globalPosition
+            jailFlag, updatedPosition = chanceSpace(globalPosition)
+            if (currentPosition != updatedPosition):
+                globalPosition = updatedPosition
+                boardStats[globalPosition] += 1
+
+
+        if (globalPosition == 2 or globalPosition == 17 or globalPosition == 33): #Chest Cards
+            currentPosition = globalPosition
+            jailFlag, updatedPosition = chestSpace(globalPosition)
+            if (currentPosition != updatedPosition):
+                globalPosition = updatedPosition
+                boardStats[globalPosition] += 1
+        
+        if (globalPosition == 30):
+            globalPosition = 10
+            inJailCount += 1
+            jailFlag = True
+        
+        turnCount += 1
+
+        if (jailFlag == True):
+            for i in range(3):
+                equalFlag, rollResult = diceRoll()
+                if (equalFlag == True):
+                    break
+            if (equalFlag == False):
+                equalFlag, rollResult = diceRoll()
+            globalPosition += rollResult     
+            boardStats[globalPosition] += 1      
 
     print("----Simulation Complete----")
     print("Turns:", turnCount)
-    print("Game Board Spread:", gameBoard)
-    print("Dice Roll Spread", diceRollStats)
-    print("Exporting Game Board Data")
+    print("Game Board Spread:", boardStats)
+    print("Dice Roll Spread", diceStats)
 
     #Track number of times jail visits were as "inmates"
-    gameBoard[40] = inJailCount 
+    boardStats[40] = inJailCount 
 
-    (pd.DataFrame.from_dict(data=gameBoard, orient='index').to_csv(f'gameBoard_Results_{simulationCount}_rounds_{turnCount}_turns.csv', header=False))
+    print("Exporting Game Board Data")
+    (pd.DataFrame.from_dict(data=boardStats, orient='index').to_csv(f'gameBoard_Results_{simulationCount}_rounds_{turnCount}_turns.csv', header=False))
     print("Exporting Dice Roll Data")
-    (pd.DataFrame.from_dict(data=diceRollStats, orient='index').to_csv(f'diceRoll_Results_{simulationCount}_rounds_{turnCount}_turns.csv', header=False))
+    (pd.DataFrame.from_dict(data=diceStats, orient='index').to_csv(f'diceRoll_Results_{simulationCount}_rounds_{turnCount}_turns.csv', header=False))
     
